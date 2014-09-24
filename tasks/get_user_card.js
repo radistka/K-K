a8b9335f88a274fd1b71ebaa5284ef96330f9d53
radistka <radistka.cat.net@gmail.com>
2014-09-24 12:31:32 +0300

    Update get_user_card.js

--- tasks/get_user_card.js
+++ tasks/get_user_card.js
@@ -3,7 +3,6 @@ var async = require('asyncawait/async');
 var await = require('asyncawait/await');
 
 module.exports = async(function(options) {
-<<<<<<< HEAD:tasks/get_user_card
     var card = await(function(done) {
         redis.spop('table:' + options.tableId + ':deck', done);
     });
@@ -11,11 +10,4 @@ module.exports = async(function(options) {
         redis.sadd('user:' + options.player.id + ':cards', card, done)
     });
     console.log('cardToUser: ' + card)
-=======
-    options.players.forEach(function(player) {
-        var card = await( redis.spop.bind(redis, 'table:' + options.tableId + ':deck'));
-        await(redis.sadd.bind(redis, 'user:' + player.id + ':cards', card));
-        console.log('cardToUser: ' + card)
-    })
->>>>>>> b1938458751949b4df8ed60328880ac48156965a:tasks/get_user_card.js
 });
